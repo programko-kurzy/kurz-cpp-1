f99b1d462546cc5a84b4e47e36b3446ab586516e
matejPeluha <matej.peluha@gmail.com>
2021-06-03 21:04:16 +0200

    ADD: new chapter

---
+++ 6.kapitola-hard/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+    std::cout << "Zadaj svoje cele meno: ";
+    std::string name;
+    getline(std::cin, name);
+
+    std::cout << "Teraz zadaj pocet opakovani: ";
+    int count;
+    std::cin >> count;
+
+    if(count < 1){
+        std::cout << "Neplatny pocet opakovani";
+    }
+    else{
+        for(int index = 0; index < count; index++){
+            std::cout << name << "\n";
+        }
+    }
+
+    return 0;
+}
